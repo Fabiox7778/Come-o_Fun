d13b12b09eb7c3879a13e7ae44ee41bbd7b68f8d
Fabiox7778 <fabiorabello08@gmail.com>
2025-05-30 16:00:17 -0300

    É par?

---
+++ ex_03.js
@@ -0,0 +1,11 @@
+function ehPar(num) {
+    let ehParMesmo = 0;
+    if (num % 2 == 0) {
+        ehParMesmo = true;
+    } else {
+        ehParMesmo = false;
+    }
+    console.log(ehParMesmo);
+}
+
+ehPar(22);
