c2b2ce1f2be4cf21de96256e2221ca9bf32df727
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-06-05 21:20:55 -0300

    Conta caracteres

--- ex_06.js
+++ ex_06.js
@@ -1,7 +1,7 @@
 function contarCaracteres(frase) {
-    let arr = frase.toString().split("")
-    let contador = arr.length
-    console.log(contador)
+    let arr = frase.toString().split("");
+    let contador = arr.length;
+    console.log(contador);
 }
 
-contarCaracteres("Top Demais!")
+contarCaracteres("Top Demais!");
